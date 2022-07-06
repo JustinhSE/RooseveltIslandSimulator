7d3de05681aa8e50e9ce223a26c647870e97b93b
justinh.tech1@gmail.com <justinh.tech1@gmail.com>
2022-07-06 14:21:21 -0400

    Stitch Cameras

---
+++ Assets/firstcamera.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstcamera : MonoBehaviour
+{
+
+        public Camera firstPersonCamera;
+        public Camera overheadCamera;
+
+        // Call this function to disable FPS camera,
+        // and enable overhead camera.
+        public void ShowOverheadView()
+        {
+            firstPersonCamera.enabled = false;
+            overheadCamera.enabled = true;
+        }
+
+        // Call this function to enable FPS camera,
+        // and disable overhead camera.
+        public void ShowFirstPersonView()
+        {
+            firstPersonCamera.enabled = true;
+            overheadCamera.enabled = false;
+        }
+    
+}
