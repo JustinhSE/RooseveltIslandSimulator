f2f940fdf5ac7cff76ae106f4131428f8d167c09
dliu <dliu30@stuy.edu>
2022-07-26 15:28:57 -0400

    put car into roosevelt island

--- Assets/Scripts/CameraTracker.cs
+++ Assets/Scripts/CameraTracker.cs
@@ -6,9 +6,14 @@ public class CameraTracker : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset;
+    Camera cam;
+    public int width = 256;
+    public int height = 256;
+    public bool flipHorizontal;
 
     void Start()
     {
+        cam = GetComponent<Camera>();
         offset = transform.position - player.transform.position;
     }
 
@@ -16,4 +21,16 @@ public class CameraTracker : MonoBehaviour
     {
         transform.position = player.transform.position + offset;
     }
+
+    void OnPreCull()
+    {
+        cam.ResetWorldToCameraMatrix();
+        cam.ResetProjectionMatrix();
+        Vector3 scale = new Vector3(flipHorizontal ? -1 : 1, 1, 1);
+        cam.projectionMatrix = cam.projectionMatrix * Matrix4x4.Scale(scale);
+    }
+
+    void OnPreRender() { GL.invertCulling = flipHorizontal; }
+
+    void OnPostRender() { GL.invertCulling = false; }
 }
